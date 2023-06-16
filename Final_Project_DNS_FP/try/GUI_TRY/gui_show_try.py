c58b272f986abb5d3c44428c75654029d1f5ee22
giladliv <giladliv123@gmail.com>
2023-06-16 21:39:28 +0300

    test passed in good manners

--- Final_Project_DNS_FP/try/GUI_TRY/gui_show_try.py
+++ Final_Project_DNS_FP/try/GUI_TRY/gui_show_try.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import tkinter as tk
+from tkinter import ttk
+
 from prog_set import *
 
 class RunMe:
@@ -8,14 +10,23 @@ class RunMe:
         toplevel1 = tk.Tk() if master is None else tk.Toplevel(master)
         toplevel1.configure(height=200, width=200)
         toplevel1.geometry("800x600")
-        prog_frame_1 = ProgressSet(toplevel1)
-        prog_frame_2 = ProgressSet(toplevel1)
-        prog_frame_3 = ProgressSet(toplevel1)
+        self.prog_frame_1 = ProgressSet(toplevel1, jump=10)
+        self.prog_frame_2 = ProgressSet(toplevel1, jump=15)
+        self.prog_frame_3 = ProgressSet(toplevel1, jump=20)
+        button3 = ttk.Button(toplevel1)
+        button3.configure(text='press me')
+        button3.pack(side="top")
+        button3.configure(command=self.push_all)
         # Main widget
         self.mainwindow = toplevel1
 
     def run(self):
         self.mainwindow.mainloop()
+
+    def push_all(self):
+        self.prog_frame_1.update_bar()
+        self.prog_frame_2.update_bar()
+        self.prog_frame_3.update_bar()
 
 
 if __name__ == "__main__":
--- Final_Project_DNS_FP/try/GUI_TRY/prog_set.py
+++ Final_Project_DNS_FP/try/GUI_TRY/prog_set.py
@@ -9,8 +9,10 @@ class ProgressSet:
 
     def __init__(self, master, max_size: int = 100, jump: int = 1, title: str = 'title'):
         # build ui
-        self.max_size = max_size
-        self.jump = jump
+        # TODO: check validity
+        self.jump = 0
+        self.max_size = 0
+
         self.title_lable = ttk.Label(master)
         self.title_lable.configure(text=title)
         self.title_lable.pack(side="top")
@@ -22,25 +24,48 @@ class ProgressSet:
             length=600,
             orient="horizontal",
             value=0,
-            maximum=self.max_size,
             variable=self.prog_bar)
         self.progressbar1.pack(side="left")
         self.prog_label = ttk.Label(frame1)
-        self.prog_label.configure(text='    0%')
+        self.prog_label.configure(text='')
         self.prog_label.pack(side="right")
         frame1.pack(side="top")
         self.mark_label = ttk.Label(master)
-        self.mark_label.configure(text='mark')
+        self.mark_label.configure(text='')
         self.mark_label.pack(side="top")
+        #set the prog
+        self.set_progress(max_size, jump)
 
-    def make_move_proccess(self, n: int):
+    def set_progress(self, max_size: int = 100, jump: int = 1):
+        self.max_size = abs(max_size)
+        self.jump = abs(jump)
+        self.progressbar1['maximum'] = self.max_size
+        self.prog_bar.set(0)
+        self.prog_label['text'] = self.FORMAT_PERC.format(0)
+        self.mark_label['text'] = self.FORMAT_MARK.format('- / -')
+    def __make_move_proccess(self, n: int):     # for jumping in several options
         curr_pos = self.prog_bar.get()          # get the current position
         if n >= 0:
-            curr_pos = min(curr_pos + n, self.max_size)
+            curr_pos = min(curr_pos + n, self.max_size)     # for every positive move update and take the lowest
         else:
-            curr_pos = max(curr_pos + n, 0)
+            curr_pos = max(curr_pos + n, 0)     # for every negative move update and take the highest (0 not changing)
+        self.__set_position(curr_pos)
+
+    def __set_position(self, curr_pos: int):
+        if curr_pos < 0 or curr_pos > self.max_size:
+            return False
+        # update percentage
         self.prog_label['text'] = self.FORMAT_PERC.format(self.get_percent_fixed(curr_pos, self.max_size))
+        # update how many completed
+        self.mark_label['text'] = self.FORMAT_MARK.format(f'{curr_pos} / {self.max_size}')
         self.prog_bar.set(curr_pos)
+        return True
+
+
+    # update bar for move
+    def update_bar(self):
+        self.__make_move_proccess(self.jump)
+
 
     @staticmethod
     def get_percent_fixed(n, max_num: int = 100, after_dot=2):
